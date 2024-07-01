efcd2b8efca1e88833749699dc9f66570481f1b2
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2024-07-01 10:10:58 +0200

    pluh

---
+++ src/components/SkillsComp.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="container">
+      <div class="row pb-4 text-center text-md-start" data-aos="fade-up" data-aos-duration="1000">
+        <div class="col-lg-8">
+          <h6 class="text-brand">Skills</h6>
+          <h1>My Skills</h1>
+        </div>
+      </div>
+  
+      <div class="row gy-4" v-if="Skills?.length">
+  
+        <div class="col-md-4" v-for="x in Skills" :key="x.id" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
+            <div class="d-flex align-items-baseline">
+                <div class="iconbox mb-4 rounded-4 me-2">
+                  <i :class="x.logo"></i>
+                </div>
+                <h3 class="mb-4">{{ x.name }}</h3>
+              </div>
+        </div>
+      </div>
+      <div v-else>
+        <p>Loading...</p>
+    </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { computed, onMounted } from 'vue'
+  import { useStore } from 'vuex'
+  const store = useStore()
+  const Skills = computed(() => store.state.Skills)
+  onMounted(() => {
+    store.dispatch('fetchSkills')
+    console.log(Skills);
+  
+  })
+  </script>
+  
+  <style scoped>
+  /* Base styling on Header tags */
+  h1,h2,h3,h4,h5,h6{
+      color: var(--Color-Header-Blossom);
+      font-weight: 700;
+    }
+   /* btn */
+   .btn{
+      padding: 12px 28px;
+      font-weight: 700;
+      color: var(--Color-Text-Fondant);
+    }
+  
+    .btn-brand{
+      background-color: var(--Color-Accent-Pinot);
+      color: var(--Color-Header-Blossom);
+      border-color: var(--Color-Header-Blossom);
+      &:hover,
+       :focus{
+        background-color: var(--Color-Background-Charcoal);
+        color: var(--Color-Header-Blossom);
+        border-color: var(--Color-Header-Blossom);
+       }
+    }
+    /* Base styling on achor tag */
+    a{
+      text-decoration: none;
+      color: var(--Color-Text-Fondant);
+      transition: all 0.4s ease;
+      font-family: var(--Font-Main-Hammersmith);
+    }
+    a:hover{
+      color: var(--Color-Header-Blossom);
+    }
+  
+  </style>
+  
--- src/store/index.js
+++ src/store/index.js
@@ -1,150 +1,150 @@
-import { createStore } from 'vuex'
+import {
+  createStore
+} from 'vuex'
 import axios from 'axios'
 import Swal from 'sweetalert2/dist/sweetalert2'
 
 const portURL = "https://imaraansheldon.github.io/EOMP/data"
 
 export default createStore({
-  state: {
-    Home : null,
-    Services_Head : null,
-    Services : null,
-    Work : null,
-    About : null,
-    Education: null,
-    Experience: null,
-    Skills_Head : null,
-    Skills : null,
-    Testimonials_Head : null,
-    Testimonials : null,
-  },
-  getters: {
-  },
-  mutations: {
-    setHome(state, value){
-      state.Home = value
-    },
-    setServices_Head(state, value){
-      state.Services_Head = value
-    },
-    setServices(state, value){
-      state.Services = value
-    },
-    setWork(state, value){
-      state.Work = value
-    },
-    setAbout(state, value){
-      state.About = value
-    },
-    setEducation(state, value){
-      state.Education = value
-    },
-    setExperience(state, value){
-      state.Experience = value
-    },
-    setSkills_Head(state, value){
-      state.Skills_Head = value
-    },
-    setSkills(state, value){
-      state.Skills = value
-    },
-    setTestimonials(state, value){
-      state.Testimonials = value
-    },
-    setTestimonials_Head(state, value){
-      state.Testimonials_Head = value
-    },
-  },
-  actions: {
-    // Home or Landing Page
-    async fetchHome(context){
-      let {Home} = await (await axios.get(portURL)).data
-      context.commit("setHome", Home)
-    },
-    async fetchServices_Head(context) {
-      // Services Header
-      try {
-        const { Services_Head } = await (await axios.get(portURL)).data
-        context.commit('setServices_Head', Services_Head);
-        // console.log(Services_Head);
-      } catch (e) {
-          Swal.fire ({
-            section_Name:"Name Not Found: Error ^(--)^",
-            section_Sub:"Sub-name Not Found: Error ^(--)^"
-          })
-      }
-    },
-    async fetchServices(context) {
-      // Services
-      try {
-        const { Services } = await (await axios.get(portURL)).data
-        context.commit('setServices', Services);
-        // console.log(Services);
-      } catch (e) {
-          Swal.fire ({
-            services_Name:"Name Not Found: Error ^(--)^",
-            services_Description:"Description Not Found: Error ^(--)^",
-            services_Type:"Type Not Found: Error ^(--)^"
-          })
-      }
-    },
-    async fetchWork(context) {
-      // Work
-      try {
-        const { Work } = await (await axios.get(portURL)).data
-        context.commit('setWork', Work);
-        // console.log(Work);
-      } catch (e) {
-          Swal.fire ({
-            services_Name:"Name Not Found: Error ^(--)^",
-            services_Description:"Description Not Found: Error ^(--)^",
-            services_Type:"Type Not Found: Error ^(--)^"
-          })
-      }
-    },
-    async fetchAbout(context) {
-      // About
-      try {
-        const { About } = await (await axios.get(portURL)).data
-        context.commit('setAbout', About);
-        console.log(About);
-      } catch (e) {
-          Swal.fire ({
-            services_Name:"Name Not Found: Error ^(--)^",
-            services_Description:"Description Not Found: Error ^(--)^",
-            services_Type:"Type Not Found: Error ^(--)^"
-          })
-      }
-    },
-    async fetchEducation(context) {
-      // Education
-      try {
-        const { Education } = await (await axios.get(portURL)).data
-        context.commit('setEducation', Education);
-        console.log(Education);
-      } catch (e) {
-          Swal.fire ({
-            services_Name:"Name Not Found: Error ^(--)^",
-            services_Description:"Description Not Found: Error ^(--)^",
-            services_Type:"Type Not Found: Error ^(--)^"
-          })
-      }
-    },
-    async fetchExperience(context) {
-      // Experience
-      try {
-        const { Experience } = await (await axios.get(portURL)).data
-        context.commit('setExperience', Experience);
-        console.log(Experience);
-      } catch (e) {
-          Swal.fire ({
-            services_Name:"Name Not Found: Error ^(--)^",
-            services_Description:"Description Not Found: Error ^(--)^",
-            services_Type:"Type Not Found: Error ^(--)^"
-          })
-      }
-    },
-  },
-  modules: {
-  }
-})
+      state: {
+        Home: null,
+        Services_Head: null,
+        Services: null,
+        Work: null,
+        Education: null,
+        Experience: null,
+        Skills: null,
+        Testimonials: null,
+      },
+      getters: {},
+      mutations: {
+        setHome(state, value) {
+          state.Home = value
+        },
+        setServices_Head(state, value) {
+          state.Services_Head = value
+        },
+        setServices(state, value) {
+          state.Services = value
+        },
+        setWork(state, value) {
+          state.Work = value
+        },
+        setEducation(state, value) {
+          state.Education = value
+        },
+        setExperience(state, value) {
+          state.Experience = value
+        },
+        setSkills(state, value) {
+          state.Skills = value
+        },
+        setTestimonials(state, value) {
+          state.Testimonials = value
+        },
+      },
+      actions: {
+        // Home or Landing Page
+        async fetchHome(context) {
+          let {Home} = await (await axios.get(portURL)).data
+          context.commit("setHome", Home)
+        },
+        async fetchServices_Head(context) {
+          // Services Header
+          try {
+            const {
+              Services_Head
+            } = await (await axios.get(portURL)).data
+            context.commit('setServices_Head', Services_Head);
+            // console.log(Services_Head);
+          } catch (e) {
+            Swal.fire({
+              section_Name: "Name Not Found: Error ^(--)^",
+              section_Sub: "Sub-name Not Found: Error ^(--)^"
+            })
+          }
+        },
+        async fetchServices(context) {
+          // Services
+          try {
+            const {
+              Services
+            } = await (await axios.get(portURL)).data
+            context.commit('setServices', Services);
+            // console.log(Services);
+          } catch (e) {
+            Swal.fire({
+              services_Name: "Name Not Found: Error ^(--)^",
+              services_Description: "Description Not Found: Error ^(--)^",
+              services_Type: "Type Not Found: Error ^(--)^"
+            })
+          }
+        },
+        async fetchWork(context) {
+          // Work
+          try {
+            const {
+              Work
+            } = await (await axios.get(portURL)).data
+            context.commit('setWork', Work);
+            // console.log(Work);
+          } catch (e) {
+            Swal.fire({
+              services_Name: "Name Not Found: Error ^(--)^",
+              services_Description: "Description Not Found: Error ^(--)^",
+              services_Type: "Type Not Found: Error ^(--)^"
+            })
+          }
+        },
+        async fetchSkills(context) {
+          try {
+            const {
+              Skills
+            } = await (await axios.get(portURL)).data
+            context.commit('setSkills', Skills);
+            console.log(Skills);
+          } catch (e) {
+            Swal.fire({
+              services_Name: "Name Not Found: Error ^(--)^",
+              services_Description: "Description Not Found: Error ^(--)^",
+              services_Type: "Type Not Found: Error ^(--)^"
+            })
+          }
+        },
+        async fetchEducation(context) {
+          // Education
+          try {
+            const {
+              Education
+            } = await (await axios.get(portURL)).data
+            context.commit('setEducation', Education);
+            console.log(Education);
+          } catch (e) {
+            Swal.fire({
+              services_Name: "Name Not Found: Error ^(--)^",
+              services_Description: "Description Not Found: Error ^(--)^",
+              services_Type: "Type Not Found: Error ^(--)^"
+            })
+          }
+        },
+        async fetchExperience(context) {
+          // Experience
+          try {
+            const {
+              Experience
+            } = await (await axios.get(portURL)).data
+            context.commit('setExperience', Experience);
+            console.log(Experience);
+          } catch (e) {
+            Swal.fire({
+              services_Name: "Name Not Found: Error ^(--)^",
+              services_Description: "Description Not Found: Error ^(--)^",
+              services_Type: "Type Not Found: Error ^(--)^"
+            })
+          }
+        }
+      },
+      modules: {
+
+      }})
